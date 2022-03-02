4c399c29f37e11e4c32dd4c932dba24656689bad
luvycunanan <luvy.cun@gmail.com>
2022-03-01 22:25:33 -0800

    tabs module done

--- tabs/app.js
+++ tabs/app.js
@@ -1,34 +1,41 @@
-// function show() {
-//   document.querySelectorAll('.article').forEach(article => {
-//     if (article.id == '') {
-//       article.id = 'hidden'
-//     } else {
-//       article.id = ''
-//     }
-//   })
-// }
-
-// let counter = 1;
-// function count() {
-//   if (counter < 4) {
-//     counter += 1;
-//   } else {
-//     counter = 1;
-//   }
-// }
-
-
 const mike = document.querySelector('.mike');
 const dustin = document.querySelector('.dustin');
 const lucas = document.querySelector('.lucas');
 const eleven = document.querySelector('.eleven');
-const elevenArticle = document.querySelector('.eleven-article');
 
-function check() {
+const elevenArticle = document.querySelector('.eleven-article');
+const mikeArticle = document.querySelector('.mike-article');
+const dustinArticle = document.querySelector('.dustin-article');
+const lucasArticle = document.querySelector('.lucas-article');
+
+function checkEleven() {
   if (eleven.id == 'active') {
     elevenArticle.id = ''
   } else {
     elevenArticle.id = 'hidden'
+  }
+}
+function checkMike() {
+  if (mike.id == 'active') {
+    mikeArticle.id = ''
+  } else {
+    mikeArticle.id = 'hidden'
+  }
+}
+
+function checkDustin() {
+  if (dustin.id == 'active') {
+    dustinArticle.id = ''
+  } else {
+    dustinArticle.id = 'hidden'
+  }
+}
+
+function checkLucas() {
+  if (lucas.id == 'active') {
+    lucasArticle.id = ''
+  } else {
+    lucasArticle.id = 'hidden'
   }
 }
 
@@ -38,7 +45,10 @@ document.querySelectorAll('.title').forEach(item => {
       item.id = 'inactive'
     })
     item.id = 'active'
-    check()
+    checkEleven()
+    checkDustin()
+    checkLucas()
+    checkMike()
   })
 })
 
